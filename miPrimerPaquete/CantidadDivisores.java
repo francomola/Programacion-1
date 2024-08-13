92a7f4fb0929ab8147a83de5cd2a61aa02bd643a
Franco_Molina <144183861+francomola@users.noreply.github.com>
2024-08-13 18:06:56 -0300

    Add files via upload

---
+++ miPrimerPaquete/CantidadDivisores.java
@@ -0,0 +1,11 @@
+package miPrimerPaquete;
+/*Ejercicio 1:
+a) Implementar la función cantDivisores que tome un parámetro de tipo entero
+y calcule la cantidad de divisores de ese entero.
+b) Averiguar cuántos divisores tienen los siguientes números: 45, 2374 y 3529.*/
+
+public class CantidadDivisores{
+    public static void cantDivisores(int divisores){
+        
+    }
+}
