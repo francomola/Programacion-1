a5c96c76c48b2dd9b923e9154b65c1e9e3cd599f
Franco_Molina <144183861+francomola@users.noreply.github.com>
2024-08-16 20:55:11 -0300

    Ejercicio1.java

--- practica0/Ejercicio1.java
+++ practica0/Ejercicio1.java
@@ -3,6 +3,6 @@ package practica0;
 Escribir el programa “¡Hola, mundo!”.*/
 public class Ejercicio1 {
     public static void main (String [] args) {
-        System.out.println ("Hola mundo");
+        System.out.println ("¡Hola, mundo!");
     }
 }
