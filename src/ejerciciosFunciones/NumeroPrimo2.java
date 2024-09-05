afedeb751ef5c25e09647c2bc634dfedc15782ea
Franco Molina <francomolina2605@gmail.com>
2024-09-05 14:50:35 -0300

    Resolviendo conflicto

---
+++ src/ejerciciosFunciones/NumeroPrimo2.java
@@ -0,0 +1,6 @@
+package ejerciciosFunciones;
+/*Ejercicio 3:
+a) Implementar la función siguientePrimo que dado un numero entero, retorne el primer número primo mayor a él.
+b) Calcular los siguientes primos de 8, 230 y 3527.*/
+
+public class NumeroPrimo2 {}
---
+++ src/ejerciciosString/StringIguales.java
@@ -0,0 +1,16 @@
+package ejerciciosString;
+/*3. Hacer una función que se llame public static boolean sonIguales(String s1, String s2)
+y devuelva true si ambos String son iguales. No usar el método .equals().*/
+public class StringIguales {
+    public static void main (String [] args) {
+//        System.out.print (sonIguales("re", "redt"));
+    }
+//    public static Boolean sonIguales (String s1, String s2) {
+//        for (int i = 0; i < s1.length(); i++) {
+//            if (s1.charAt(i) == s2.charAt(i)) {
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
+}
