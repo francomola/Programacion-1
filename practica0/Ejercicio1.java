478b329bbf7ce5a677c210bde6e3f088a5c4acb1
Franco_Molina <144183861+francomola@users.noreply.github.com>
2024-09-06 09:42:03 -0300

    Delete practica0 directory

--- practica0/Ejercicio1.java
+++
@@ -1,8 +0,0 @@
-package practica0;
-/*Ejercicio 1
-Escribir el programa “¡Hola, mundo!”.*/
-public class Ejercicio1 {
-    public static void main (String [] args) {
-        System.out.println ("¡Hola, mundo!");
-    }
-}
