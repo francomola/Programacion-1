f9af44e9be133521753089237064ac5e2f440a42
Franco Molina <francomolina2605@gmail.com>
2024-09-07 19:06:30 -0300

    Añadiendo segundo ejercicio de números primos

--- src/ejerciciosFunciones/NumeroPrimo2.java
+++ src/ejerciciosFunciones/NumeroPrimo2.java
@@ -3,4 +3,17 @@ package ejerciciosFunciones;
 a) Implementar la función siguientePrimo que dado un numero entero, retorne el primer número primo mayor a él.
 b) Calcular los siguientes primos de 8, 230 y 3527.*/
 
-public class NumeroPrimo2 {}
+public class NumeroPrimo2 {
+    public static void main (String [] args) {
+        System.out.print (siguientePrimo(8));
+        System.out.print (siguientePrimo(230));
+        System.out.print (siguientePrimo(3527));
+    }
+    static int siguientePrimo (int num) {
+        num++;
+        while (!NumeroPrimo.esPrimo(num)) {
+            num++;
+        }
+        return num;
+    }
+}
